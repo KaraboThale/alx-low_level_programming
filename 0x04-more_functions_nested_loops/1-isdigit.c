70d64628ff236778536dfe68ac8159138b4f00e0
KaraboThale <karabob.thale@gmail.com>
2023-09-15 16:32:04 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,11 @@
 
 /**
 *_isdigit - Check numbers 0-9
-*
 *@c: Char to be checked
-*
 *Return: (1) for digit (0) if not
 */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);

b10228554413645d874386158f22da62efa97151
KaraboThale <karabob.thale@gmail.com>
2023-09-15 19:22:17 -0700

    Print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,22 +5,18 @@
 *@size: size of the square 
 */
 
-
 void print_square(int size)
 {
 	if (size <= 0)
-	{
 		_putchar('\n');
-	} else
+	else
 	{
 		int i, j;
 
-		for (i =0; i < size; i++)
+		for (i =1; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
+			for (j = 1; j < size; j++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}

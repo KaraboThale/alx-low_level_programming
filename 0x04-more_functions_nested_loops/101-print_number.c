9f1701438751bb0c51d502d9650431c320d5a628
KaraboThale <karabob.thale@gmail.com>
2023-09-15 19:57:47 -0700

    Print an interger

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,9 +2,7 @@
 
 /**
 *print_number - prints an interger
-*
 *@n: interger to be printed
-*
 */
 void print_number(int n)
 {
@@ -23,6 +21,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	
-	_putchar(n1 % 10 + '0');
+	_putchar((n1 % 10) + '0');
 }

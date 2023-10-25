c20d31877bb0571d97f755f4d2dc1c2a85d80306
KaraboThale <karabob.thale@gmail.com>
2023-10-25 15:24:43 -0700

    Return the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,12 +14,11 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y % 2 == 0)
+	if (y == 0)
 	{
-		return (_pow_recursion(x * x, y / 2));
+		return (1);
 	}
-	else
 	{
-		return (x * _pow_recursion(x * x, y / 2));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }

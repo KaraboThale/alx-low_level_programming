2e3ad9cce5c3cbf5efc8653e42e5adaafe21a997
KaraboThale <karabob.thale@gmail.com>
2023-11-07 11:27:58 -0800

    Write a function that frees dogs.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen - returns string length.
- * @s: string.
+ * @s: string to evaluate.
  * Return: length of the string.
  */
 
@@ -38,10 +38,12 @@ char *_strcpy(char *dest, char *src)
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 
 	return (dest);
 }
@@ -61,20 +63,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	dog = malloc(sizeof(dog_t));
 
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
-
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	
 	if (dog->owner == NULL)
 	{
 		free(dog);

8f62c7bb150bfc5304e4dd83d8eff3501e3d2fd9
KaraboThale <karabob.thale@gmail.com>
2023-11-08 13:39:39 -0800

    Write a program that prints the opcodes of its own main function.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - returns the sum of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: sum of two numbers.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: difference of two numbers.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: product of two numbers.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of dividing two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: result of division of two numbers.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of division of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: remainder of the division of two numbers.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
